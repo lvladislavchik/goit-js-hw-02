6a0152732b78051bb24f9a91f355ff73f7b2d7ac
lvladislavchik <97337116+lvladislavchik@users.noreply.github.com>
2023-12-10 16:00:39 +0200

    completed

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,14 @@
-function textConvert(value) {
-  return Number.parseFloat(value.replace(',', '.'));
+function checkForSpam(message) {
+  if (message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale') === true) {
+    return true;
+  }
+  return false;
 }
 
-function getElementWidth(content, padding, border) {
-  content = textConvert(content);
-  padding = textConvert(padding);
-  border = textConvert(border);
-  const elementWidth = content + padding * 2 + border * 2;
-  return elementWidth;
-}
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
